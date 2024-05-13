a70865ebe922e631dba9430b565c4bfa07cd1b8a
isllamkhalil <118750615+isllam-colon@users.noreply.github.com>
2024-05-13 14:35:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const element = document.querySelector('your-element-selector');
+
+element.style.color = 'red';
+element.style.fontSize = '80px';
+element.style.fontWeight = 'bold';
+element.style.textAlign = 'center';
+element.style.fontFamily = 'Arial';
+
